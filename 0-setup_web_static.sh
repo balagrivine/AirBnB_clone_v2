3d667d8515f979f906e1eb4bb315a317a3c66e66
balagrivine <balagrivine@gmail.com>
2024-01-31 00:29:15 -0800

    write a bash script that  sets up a web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt update
+sudo apt install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+	<head>
+	</head>
+	<body>
+          Holberton School
+	</body>
+	</html>" > /data/web_static/releases/test/index.html
+
+#create  asymbolic link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#create user and group if not already exist
+#sudo adduser ubuntu
+#sudo addgroup ubuntu
+
+#change ownership of user and group
+chown -R ubuntu:ubuntu /data/
+
+#configure nginx server to serve content
+block =  "server {
+		location /hbnb_static {
+		alias /data/web_static/current/
+	}
+}"
+
+echo "$block" | sudo tee /etc/nginx/sites-available/default
+
+#restart nginx server
+sudo service nginx restart
