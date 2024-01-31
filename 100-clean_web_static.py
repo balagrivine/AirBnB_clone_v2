cecefec01d983cc3c01bcf75316467d0fb7e987e
balagrivine <balagrivine@gmail.com>
2024-01-31 11:52:19 -0800

    Write a puppet manifest that sets up your web servers for the deployment of web_static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -60,3 +60,8 @@ def deploy():
 
     value = do_deploy(archive)
     return value
+
+def do_clean(unmber=0):
+    """Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean"""
+
+
