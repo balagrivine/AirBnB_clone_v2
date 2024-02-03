938535a03d6a777dd3e296207ad4da8e5748abd3
balagrivine <balagrivine@gmail.com>
2024-02-03 02:03:07 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 
 sudo apt update
 sudo apt install nginx
+mkdir -p /data/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "<html>
@@ -24,7 +25,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 #configure nginx server to serve content
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server/a /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 
 #restart nginx server
 sudo service nginx restart
---
+++ 101-setup_web_static.pp
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#A puppet manifest that sets up your web servers for the deployment of web_static
+
+exec { 'install nginx':
+}
